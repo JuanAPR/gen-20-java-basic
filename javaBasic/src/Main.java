05435ac6bba2ec2c012796f80751a679f81aff77
Juan Adhiasta Pratama <juanadhiastapersonal@gmail.com>
2024-02-27 15:27:55 +0700

    Java Basic Hello World

---
+++ javaBasic/src/Main.java
@@ -0,0 +1,12 @@
+//JavaBasic
+//Juan Adhiasta Pratama gen 20 pro academy
+
+public class Main { //class pada Java berfungsi sebagai blueprint objek-objek yang dibuat
+    public static void main(String[] args) { //merupakan Java main method yang menjembatani dunia statis dan dinamis java
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.println("Hello World"); //System.out.println digunakan untuk melakukan print string
+
+
+        }
+    }
